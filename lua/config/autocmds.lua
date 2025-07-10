e9a207455c7725d71c3febe7c3858bad36f326c3
noctukkkrne_mac <noctukkkrne@163.com>
2025-07-10 09:52:54 +0800

    Change the colorscheme back, noctukkkrne_20250710

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -7,7 +7,7 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
-vim.cmd[[set background=dark]]
-vim.cmd[[colorscheme gruvbox]]
+-- vim.cmd[[set background=dark]]
+-- vim.cmd[[colorscheme gruvbox]]
 -- vim.cmd[[colorscheme tokyonight-day]]
--- vim.cmd[[colorscheme catppuccin]]
+vim.cmd[[colorscheme catppuccin]]
